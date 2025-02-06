b22c97cace64fc8472df187e560dad29f459fc74
Hamza-Yaseen1 <hamza.sce1@gmail.com>
2025-02-06 19:51:06 +0500

    Update Contact page layout and enhance card details

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -18,30 +18,45 @@ const Contact = () => {
           <br /><br /><br />
           <div className="cards grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 place-items-center">
   {/* Card 1 */}
-  <div className="card w-52 h-56 rounded-xl border border-blue-500 bg-gradient-to-br from-blue-100 to-blue-300 shadow-lg hover:shadow-2xl transition-transform duration-300 hover:scale-105">
-    <div className="h-full flex items-center justify-center text-blue-800 font-semibold">
-      Card 1
+  <div className="card w-52 h-56 rounded-xl border shadow-lg hover:shadow-2xl transition-transform duration-300 hover:scale-105">
+    <div className="h-full">
+      <h1 className="p-9 text-xl font-bold pb-0">Address</h1>
+      <h2 className="p-4 pt-3 pl-5 text-md font-semibold">514 Magnolia St. Orlando, FL 32806</h2>
+      <button className="px-3 py-2 rounded border border-orange-600 bg-white hover:bg-orange-600 ml-5 font-semibold text-orange-600 hover:text-white transition duration-300 ease-in-out">Get direction</button>
     </div>
   </div>
 
   {/* Card 2 */}
-  <div className="card w-52 h-56 rounded-xl border border-green-500 bg-gradient-to-br from-green-100 to-green-300 shadow-lg hover:shadow-2xl transition-transform duration-300 hover:scale-105">
-    <div className="h-full flex items-center justify-center text-green-800 font-semibold">
-      Card 2
+  <div className="card w-52 h-56 rounded-xl  shadow-lg hover:shadow-2xl transition-transform duration-300 hover:scale-105">
+    <div className="h-full">
+      <h1 className="text-xl font-bold p-9 pb-0 pl-14">Phone</h1>
+      <h2 className="font-semibold pl-5 pt-4 text-sm text-gray-600">Main office</h2>
+      <span title="+1 234-567-890" className=" pl-5">+1 234-444-123</span>
+      <h2 className="font-semibold pl-5 pt-4 text-sm text-gray-600">Main office</h2>
+      <span title="+1 234-567-890" className="pl-5">+1 234-555-124</span>
     </div>
   </div>
 
   {/* Card 3 */}
-  <div className="card w-52 h-56 rounded-xl border border-red-500 bg-gradient-to-br from-red-100 to-red-300 shadow-lg hover:shadow-2xl transition-transform duration-300 hover:scale-105">
-    <div className="h-full flex items-center justify-center text-red-800 font-semibold">
-      Card 3
+  <div className="card w-52 h-56 rounded-xl border  shadow-lg hover:shadow-2xl transition-transform duration-300 hover:scale-105">
+  <div className="h-full">
+      <h1 className="text-xl font-bold p-9 pb-0 pl-14">Schedule</h1>
+      <h2 className="font-semibold pl-5 pt-4 text-sm text-gray-600">Mon - Thu</h2>
+      <span title="+1 234-567-890" className=" pl-5  font-bold">10:00 - 22:00</span>
+      <h2 className="font-semibold pl-5 pt-4 text-sm text-gray-600">Fri - Sat
+      </h2>
+      <span title="+1 234-567-890" className="pl-5  font-bold">10:00 - 18:00</span>
     </div>
   </div>
 
   {/* Card 4 */}
-  <div className="card w-52 h-56 rounded-xl border border-purple-500 bg-gradient-to-br from-purple-100 to-purple-300 shadow-lg hover:shadow-2xl transition-transform duration-300 hover:scale-105">
-    <div className="h-full flex items-center justify-center text-purple-800 font-semibold">
-      Card 4
+  <div className="card w-52 h-56 rounded-xl border  shadow-lg hover:shadow-2xl transition-transform duration-300 hover:scale-105">
+  <div className="h-full">
+      <h1 className="text-xl font-bold p-9 pb-0 pl-14">Email</h1>
+      <h2 className="font-semibold pl-5 pt-4 text-sm text-gray-600">Main office</h2>
+      <span title="+1 234-567-890" className=" pl-5 font-semibold">office@example.com</span>
+      <h2 className="font-semibold pl-5 pt-4 text-sm text-gray-600">Reception room      </h2>
+      <span title="+1 234-567-890" className="pl-5 font-semibold">reception@example.com</span>
     </div>
   </div>
 </div>
@@ -50,7 +65,8 @@ const Contact = () => {
 <div className="contact-details mt-16  w-4/5 m-auto  shadow-2xl">
  <div className="content p-12">
  <h1 className="text-4xl font-bold text-center">Let&apos;s Contact US</h1>
- <p className="text-xl font-semibold text-center p-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit, <br /> sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+ <p className="text-xl md:font-semibold sm:font-medium  text-center sm:p-0  md:p-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+ </p>
  </div>
  <ContactForm/>
 </div>
